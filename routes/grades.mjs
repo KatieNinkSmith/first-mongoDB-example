be646c010e7589a36ce92d43f499ac819088be83
Katie Nink Smith <katiej.smith@outlook.com>
2024-12-03 07:25:39 -0700

    additional notes for later reference

--- routes/grades.mjs
+++ routes/grades.mjs
@@ -4,6 +4,29 @@ import db from "../db/conn.mjs";
 
 const router = express.Router();
 
+// full CRUD functionality
+// c - create -post
+// r - read   -get
+// u - update -out/patch
+// d - delete - delete
+
+// for read we usually do an index route and a show route
+// index displays or gets many db items// show displays one, usually based on the id
+
+// --------------------------------------------------
+//                        all route
+// --------------------------------------------------
+
+// =========== get implements READ funtionality
+// we want to be carefull with this get route
+// because it could be a huge amount of data
+// that is why we limit to 50 in this example
+// if you wanted to either use pagination or
+// somehow iterate through,
+// you might want to use limit(num) and skip(num)
+// ==== make sure you are using async and await
+// because bd access requests are asynchronous,
+// but we need that data before we move on
 router.get("/", async (req, res) => {
   let collection = await db.collection("grades");
   let results = await collection.find({}).limit(50).toArray();
